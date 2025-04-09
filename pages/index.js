fd888f6b47e8e4f2b1c39df371faa4ace07084b0
Willian Paulino <paulino.willian@bcg.com>
2025-04-09 18:07:14 -0300

    project initialization

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                This is the home page.
+            </p>
+        </div>
+    )
+}
+
+export default Home;
