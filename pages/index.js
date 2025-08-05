9152536333f5e6c514242a1281d1904c5a7b132b
Willian Paulino <willianpaulinox@gmail.com>
2025-08-05 11:42:23 -0300

    running project again

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
         <div>
             <h1>Oi Vidinha</h1>
             <p>
-                Se me ama, da uma risadinha.
+                Se me ama, da uma risadinha, teste.
             </p>
         </div>
     )
